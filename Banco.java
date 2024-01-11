30c0445d724f5764c9cc66f1040379a0cd028cae
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2024-01-10 20:20:48 -0600

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -1,19 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package bancac;
 
-import bancac.Modelos.Trabajador;
-import java.util.Map;
-
-/**
- *
- * @author localadmin
- */
-public class Banco {
-
-    public void distribuirGanancias(Map<Trabajador, Double> trabajadores) {
-        // Implementa lógica para distribuir ganancias
-    }
-}
